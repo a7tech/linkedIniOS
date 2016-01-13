b6532c0374a2c3329c7aa550672fc5a0507e6690
Sourabh Soni <sourabh@inwizards.com>
2016-01-13 05:48:49 -0800

    Linked in share from iOS

---
+++ LinkedShareSDK/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LinkedShareSDK
+//
+//  Created by mac on 07/01/16.
+//  Copyright © 2016 Inwizards. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <linkedin-sdk/LISDK.h>
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LinkedShareSDK/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  LinkedShareSDK
+//
+//  Created by mac on 07/01/16.
+//  Copyright © 2016 Inwizards. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <linkedin-sdk/LISDK.h>
+@interface ViewController : UIViewController
+
+- (IBAction)LoginBtn:(id)sender;
+
+@end
+
